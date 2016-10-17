8cc27d82de7bb1f413b31887aab9e48027b4444e
Sjoerd <sjoerdvangalen93@gmail.com>
2016-10-17 16:42:37 +0200

    add_post

---
+++ .idea/add_post.php
@@ -0,0 +1,13 @@
+<html>
+
+<form method="post" action="insert.php">
+
+    <input type="text" name="postTitle" placeholder="Post Title">
+    <input type="text" name="postDesc" placeholder="Post Description">
+    <input type="submit">
+
+
+</form>
+
+</html>
+
