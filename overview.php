608cea08456a23dd74e29ce9d8d846b25f8b0cc7
Sjoerd <sjoerdvangalen93@gmail.com>
2016-10-30 21:47:42 +0100

    Done!

--- overview.php
+++ overview.php
@@ -1,14 +1,30 @@
 <?php
 include "header.php";
 
+
+//user input
+$page = isset($_GET['page']) ? (int)$_GET['page'] :1;
+$perpage = isset($_GET['per-page']) && $_GET['per-page'] ? (int)$_GET['per-page'] : 5;
+
+//postitioning
+$start = ($page > 1) ? ($page * $perpage) - $perpage : 0;
+
+//query
 $con = new Connection();
 $con = $con->connect();
-$sql = "SELECT postID, postDesc, postTitle, postDate FROM posts ORDER BY postDate DESC";
-
+$sql = "SELECT postID, postDesc, postTitle, postDate FROM posts limit $start, $perpage";
 $result = mysqli_query($con, $sql);
+$fetch = $result->fetch_assoc();
 
+// pages
+$sql_total = "SELECT * FROM posts";
+$result_total = mysqli_query($con, $sql_total);
+$fetch_total = $result_total->fetch_all();
+$total = count($fetch_total);
+$pages = ceil ($total / $perpage)
 
 ?>
+
 
 
 <html>
@@ -39,3 +55,21 @@ $result = mysqli_query($con, $sql);
     </tbody>
 </table>
 </form>
+
+<ul class = "pagination">
+
+    <?php for ($x = 1; $x <= $pages; $x++): ?>
+        <li><a href="?page=<?php echo $x; ?>&per-page=<?php echo $perpage?>"><?php echo $x?></a></li>
+    <?php endfor; ?>
+
+</ul>
+
+<div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Show items
+        <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+        <li><a href="overview.php?page=1&per-page=2">2</a></li>
+        <li><a href="overview.php?page=1&per-page=5">5</a></li>
+        <li><a href="overview.php?page=1&per-page=10">10</a></li>
+    </ul>
+</div>
--- register.php
+++ register.php
@@ -8,9 +8,12 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <html>
 <h1>Register here!</h1>
-<form method="post" action="register_insert.php">
-    <input type="text" name="username" placeholder="username">
-    <input type="password" name="password" placeholder="Password">
+<form class="col-xs-2" method="post" action="register_insert.php">
+    <div class="form-group">
+        <label for="user">Name:</label>
+        <input type="text" name="username" placeholder="username" class="form-control">
+        <label for="pass">Password:</label>
+    <input type="password" name="password" placeholder="Password" class="form-control">
     <input type="submit">
 </form>
 </html>
